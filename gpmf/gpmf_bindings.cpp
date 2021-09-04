e270a7ff29884f0c0b0e2b108b37739c308ec3f7
faalbers <chalbers@gmail.com>
2021-09-04 16:10:22 -0700

    now working

--- gpmf/gpmf_bindings.cpp
+++ gpmf/gpmf_bindings.cpp
@@ -1,5 +1,13 @@
 #include <pybind11/pybind11.h>
 #include "GPMF.hpp"
+#include <pybind11/stl.h>
+#include <pybind11/numpy.h>
+#include <vector>
+
+struct testStruct {
+    int x;
+    double y;
+};
 
 namespace py = pybind11;
 
@@ -7,4 +15,24 @@ PYBIND11_MODULE(gpmf_bindings, m)
 {
     m.doc() = "GPMF Gopro metadata library";
     
+    py::class_<GPMF::sampleEntryType>( m, "sampleEntry" )
+        .def_readonly("value", &GPMF::sampleEntryType::value)
+        .def_readonly("unit", &GPMF::sampleEntryType::unit)
+        .def_readonly("info", &GPMF::sampleEntryType::info)
+        ;
+    
+    py::class_<GPMF::sampleType>( m, "sample" )
+        .def_readonly("entries", &GPMF::sampleType::entries)
+        .def_readonly("time", &GPMF::sampleType::time)
+        .def_readonly("duration", &GPMF::sampleType::duration)
+        ;
+
+    py::class_<GPMF::GPMF>( m, "GPMF" )
+        .def(py::init<std::string>())
+        .def("printHierarchyData", &GPMF::GPMF::printHierarchyData, "print hierarchy with data",
+            py::arg("fullLists") = false)
+        .def("printHierarchy", &GPMF::GPMF::printHierarchy)
+        .def("getAcceleration", &GPMF::GPMF::getAcceleration)
+        .def("getGyroscope", &GPMF::GPMF::getGyroscope)
+        ;
 }
--- testgpmf.py
+++ testgpmf.py
@@ -1,5 +1,17 @@
-import gpmf, sys
+#import gpmf, sys
+from gpmf.gpmf_bindings import GPMF
+import sys
 
+myTest = GPMF("gopro6.mp4")
+result = myTest.getGyroscope()
+for sample in result:
+    print('%s: %2.5f %s' % (sample.time.info, sample.time.value, sample.time.unit), end='')
+    print(', %s: %2.5f %s' % (sample.duration.info, sample.duration.value, sample.duration.unit), end='')
+    for entry in sample.entries:
+        print(', %s: %2.5f %s' % (entry.info, entry.value, entry.unit), end='')
+    print()
+        
 print('GPMF test Done !')
+
 
 sys.exit(0)
